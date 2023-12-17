ad3f0437d8274bcded1431f9c8aaeff9fe0e0634
alvian120140162 <153977975+alvian120140162@users.noreply.github.com>
2023-12-17 22:56:35 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Dummy data for demonstration
 const dummyData = [
     { nim: '120140162',name: 'Alvian Manshurin', department: 'Teknik Informatika', semester: 7, gender: 'Laki-laki', status: 'Aktif' },
-    { nim: '220240262', name: 'Jane Doe', department: 'Social Engineering', semester: 9, gender: 'Perempuan', status: 'Non-aktif' },
+    { nim: '220240262', name: 'Alexandra Daddario', department: 'Model & Acting', semester: 9, gender: 'Perempuan', status: 'Non-aktif' },
 ];
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -20,7 +20,7 @@ function submitForm() {
     const status = document.querySelector('input[name="status"]:checked').value;
 
     // Validate form data
-    if (name === '' || nim === '' || department === '' || semester === '' || status === undefined) {
+    if (name === '' || nim === '' || department === '' || semester === '' || status === '') {
         alert('Mohon lengkapi semua data!');
         return;
     }
@@ -38,6 +38,7 @@ function submitForm() {
     document.getElementById('studentForm').reset();
 }
 
+// Modifikasi displayData untuk menambahkan tombol aksi
 function displayData(data) {
     const table = document.getElementById('studentTable');
 
@@ -45,7 +46,7 @@ function displayData(data) {
     table.innerHTML = '';
 
     // Create table headers
-    const headers = ['No', 'Nim', 'Nama', 'Jurusan', 'Semester', 'Jenis Kelamin', 'Status'];
+    const headers = ['No', 'Nim', 'Nama', 'Jurusan', 'Semester', 'Jenis Kelamin', 'Status', 'Aksi'];
     const headerRow = table.insertRow();
     headers.forEach(headerText => {
         const th = document.createElement('th');
@@ -66,8 +67,73 @@ function displayData(data) {
             const cell = row.insertCell();
             cell.textContent = value;
         });
+
+        // Add action buttons (edit and delete)
+        const cellActions = row.insertCell();
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.onclick = () => editStudent(index);
+        cellActions.appendChild(editButton);
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Hapus';
+        deleteButton.onclick = () => deleteStudent(index);
+        cellActions.appendChild(deleteButton);
     });
 }
+
+// Fungsi untuk mengedit data mahasiswa
+function editStudent(index) {
+    // Mendapatkan data mahasiswa berdasarkan indeks
+    const studentToEdit = dummyData[index];
+
+    // Mengisi formulir dengan data mahasiswa yang akan diedit
+    document.getElementById('nim').value = studentToEdit.nim;
+    document.getElementById('name').value = studentToEdit.name;
+    document.getElementById('department').value = studentToEdit.department;
+    document.getElementById('semester').value = studentToEdit.semester;
+    document.getElementById('gender').value = studentToEdit.gender;
+    document.querySelector(`input[name="status"][value="${studentToEdit.status}"]`).checked = true;
+
+    // Menyimpan indeks mahasiswa yang akan diedit
+    document.getElementById('editIndex').value = index;
+}
+
+// Fungsi untuk menghapus data mahasiswa
+function deleteStudent(index) {
+    // Konfirmasi pengguna sebelum menghapus
+    if (confirm('Apakah Anda yakin ingin menghapus mahasiswa ini?')) {
+        // Hapus mahasiswa dari array dummyData berdasarkan indeks
+        dummyData.splice(index, 1);
+
+        // Menampilkan data yang diperbarui di tabel
+        displayData(dummyData);
+    }
+}
+
+// Fungsi untuk menyimpan perubahan setelah pengeditan
+function saveEditedStudent() {
+    const index = document.getElementById('editIndex').value;
+
+    // Mendapatkan data yang diperbarui dari formulir
+    const nim = document.getElementById('nim').value;
+    const name = document.getElementById('name').value;
+    const department = document.getElementById('department').value;
+    const semester = document.getElementById('semester').value;
+    const gender = document.getElementById('gender').value;
+    const status = document.querySelector('input[name="status"]:checked').value;
+
+    // Memperbarui data mahasiswa di array dummyData
+    dummyData[index] = { nim, name, department, semester, gender, status };
+
+    // Menampilkan data yang diperbarui di tabel
+    displayData(dummyData);
+
+    // Mereset formulir setelah pengeditan
+    document.getElementById('studentForm').reset();
+    document.getElementById('editIndex').value = ''; // Mereset indeks pengeditan
+}
+
 
 // Fungsi untuk menetapkan cookie
 function setCookie(name, value, days) {
